a20aa334465681bb0c53fb0a6f284dcbd89e59f0
Dowson <dcheung@smail.pembinatrails.ca>
2022-06-08 11:36:41 -0500

    first commit

---
+++ src/Top.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Top () {
+    return <>
+        <h1 class="animated fadeInRightBig">2048</h1>
+        <p>Use the buttons or arrow keys to play game. Press 'N' to start a new game.</p>
+        <div id="root"></div>
+    </>
+}
